403f3c0ee0c2b00d3987bfe4a866067e41cbdffc
Ricardo Brambila <ricardo.brambila.external@bt.com>
2019-11-29 14:16:16 -0300

    chore(twitter): creates initial actions of tweet

--- packages/twitter/src/getTweet.js
+++ packages/twitter/src/getTweet.js
@@ -1,6 +1,7 @@
 import getTweetPicture from './getTweetPicture';
 import getTweetUser from './getTweetUser';
 import getTweetMessage from './getTweetMessage';
+import getTweetActions from './getTweetActions';
 
 export default function getTweet(tweet) {
   const tweetElem = document.createElement('li');
@@ -9,11 +10,13 @@ export default function getTweet(tweet) {
   const tweetPicture = getTweetPicture(tweet);
   const tweetUser = getTweetUser(tweet);
   const tweetMessage = getTweetMessage(tweet);
+  const tweetActions = getTweetActions(tweet);
 
   const div = document.createElement('div');
   div.classList.add('d-flex', 'flex-column');
   div.appendChild(tweetUser);
   div.appendChild(tweetMessage);
+  div.appendChild(tweetActions);
 
   tweetElem.appendChild(tweetPicture);
   tweetElem.appendChild(div);
---
+++ packages/twitter/src/getTweetActions.js
@@ -0,0 +1,11 @@
+import conversation from '../imgs/conversation.png'
+import retweet from '../imgs/retweet.png'
+import heart from '../imgs/heart.png'
+import upload from '../imgs/upload.png'
+
+export default function(tweet) {
+  const todo = document.createElement('p');
+  todo.innerText = "//TODO: Actions"
+
+  return todo;
+}
